6aa06bab703de1782a12dea1d369babd72542b4e
zckoh <zckoh96@gmail.com>
2021-10-24 13:57:12 +0100

    tidy up code base

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import { Button, IconButton } from "@chakra-ui/button";
+import { IconButton } from "@chakra-ui/button";
+import { Text, Link } from "@chakra-ui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useColorMode, useColorModeValue } from "@chakra-ui/color-mode";
 import {
@@ -8,8 +9,9 @@ import {
     faTimes,
     faBars,
 } from "@fortawesome/free-solid-svg-icons";
-import { Box, Flex, Link, Stack, Text } from "@chakra-ui/layout";
-import { useDisclosure } from "@chakra-ui/hooks";
+import { Spacer } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/layout";
+import NextLink from "next/link";
 import Logo from "./Logo";
 
 interface MenuToggleProps {
@@ -18,17 +20,15 @@ interface MenuToggleProps {
 }
 
 const MenuToggle = ({ toggle, isOpen }: MenuToggleProps) => {
+    const color = useColorModeValue("gray.900", "gray.100");
+
     return (
         <Box display={{ base: "block", md: "none" }} onClick={toggle}>
             <IconButton
                 as="a"
                 aria-label="menu-toggle"
-                icon={
-                    <FontAwesomeIcon
-                        icon={isOpen ? faTimes : faBars}
-                        fontSize="20px"
-                    />
-                }
+                color={color}
+                icon={<FontAwesomeIcon icon={isOpen ? faTimes : faBars} />}
             />
         </Box>
     );
@@ -44,16 +44,17 @@ const MenuItem = ({ children, isLast, to = "/", ...rest }: MenuItemProps) => {
     const color = useColorModeValue("gray.900", "gray.100");
 
     return (
-        <Box
+        <Text
             mb={{ base: isLast ? 0 : 8, sm: 0 }}
             mr={{ base: 0, sm: isLast ? 0 : 8 }}
             display="block"
+            color={color}
             {...rest}
         >
-            <Link href={to} color={color}>
-                {children}
-            </Link>
-        </Box>
+            <NextLink href={to} passHref>
+                <Link>{children}</Link>
+            </NextLink>
+        </Text>
     );
 };
 
@@ -66,55 +67,90 @@ const MenuLinks = ({ isOpen }: MenuLinksProps) => (
         display={{ base: isOpen ? "block" : "none", md: "block" }}
         flexBasis={{ base: "100%", md: "auto" }}
     >
-        <Stack
-            spacing={8}
-            align="center"
+        <Flex
+            align={["center", "center", "center", "center"]}
             justify={["center", "space-between", "flex-end", "flex-end"]}
             direction={["column", "row", "row", "row"]}
             pt={[4, 4, 0, 0]}
         >
             <MenuItem to="/">Home</MenuItem>
-            <MenuItem to="/how">How It Works</MenuItem>
-        </Stack>
+            <MenuItem to="/how">How It works </MenuItem>
+            <MenuItem to="/faetures">Features </MenuItem>
+
+            {/* <MenuItem to="/signup" isLast>
+            <Button
+                size="sm"
+                rounded="md"
+                color={[
+                    "primary.500",
+                    "primary.500",
+                    "white",
+                    "white",
+                ]}
+                bg={[
+                    "white",
+                    "white",
+                    "primary.500",
+                    "primary.500",
+                ]}
+                _hover={{
+                    bg: [
+                        "primary.100",
+                        "primary.100",
+                        "primary.600",
+                        "primary.600",
+                    ],
+                }}
+            >
+                Create Account
+            </Button>
+        </MenuItem> */}
+        </Flex>
     </Box>
 );
 
-const NavBar = (props: any) => {
-    const [isOpen, setIsOpen] = useState(false);
+const ColorToggleButton = () => {
     const { colorMode, toggleColorMode } = useColorMode();
     const color = useColorModeValue("gray.900", "gray.100");
 
+    return (
+        <IconButton
+            aria-label="color-mode-toggle"
+            onClick={toggleColorMode}
+            color={color}
+            ml={{ base: "15", md: "none" }}
+            mr={{ base: "none", md: "15" }}
+            icon={
+                <FontAwesomeIcon
+                    icon={colorMode === "light" ? faMoon : faSun}
+                />
+            }
+        />
+    );
+};
+
+const NavBar = (props: any) => {
+    const [isOpen, setIsOpen] = useState(false);
     const toggle = () => setIsOpen(!isOpen);
 
     return (
         <Flex
             as="nav"
             align="center"
-            justify="space-between"
             wrap="wrap"
             w="100%"
             mb={8}
             p={8}
-            bg={["primary.500", "primary.500", "transparent", "transparent"]}
-            color={["black", "black", "primary.700", "primary.700"]}
+            bg={["blue.500", "primary.500", "transparent", "transparent"]}
+            color={["white", "white", "primary.700", "primary.700"]}
             {...props}
         >
-            <Flex>
+            <Flex align="center">
                 <Logo />
             </Flex>
-            <IconButton
-                aria-label="color-mode-toggle"
-                onClick={toggleColorMode}
-                color={color}
-                icon={
-                    <FontAwesomeIcon
-                        icon={colorMode === "light" ? faMoon : faSun}
-                        inverse={colorMode === "light" ? false : true}
-                        fontSize="20px"
-                    />
-                }
-            />
+            <Spacer />
             <MenuToggle toggle={toggle} isOpen={isOpen} />
+            <ColorToggleButton />
             <MenuLinks isOpen={isOpen} />
         </Flex>
     );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,10 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import { Container, Flex, VStack } from "@chakra-ui/react";
-import styles from "../styles/Home.module.css";
-import Footer from "../components/Footer";
+import { Container, Flex } from "@chakra-ui/react";
 
 const Home: NextPage = () => {
     return (
         <Container maxWidth="container.xl" padding={0}>
-          asdlkj
-            <Flex h="100vh" py={20}>
-                <VStack
-                    w="full"
-                    h="full"
-                    p={10}
-                    spacing={10}
-                    alignItems="flex-start"
-                >
-                    <p>helo</p>
-                </VStack>
-                <VStack
-                    w="full"
-                    h="full"
-                    p={10}
-                    spacing={10}
-                    alignItems="flex-start"
-                    bg="gray.50"
-                >
-                    <p>helo2</p>
-                </VStack>
-            </Flex>
+            <Flex h="90vh">This will be the landing page</Flex>
         </Container>
     );
 };
